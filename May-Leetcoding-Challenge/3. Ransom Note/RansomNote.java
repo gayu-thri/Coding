068c590f15db00e0d6f74cb5c18855c7396fb61b
gayu-thri <egayu3@gmail.com>
2020-05-05 14:44:11 +0530

    Add Day3 - Set,Count/ HashMap(put,get,remove,containsKey)

--- May-Leetcoding-Challenge/3. Ransom Note/RansomNote.java
+++ May-Leetcoding-Challenge/3. Ransom Note/RansomNote.java
@@ -4,12 +4,14 @@ class Solution {
         Map<Character, Integer> freq = new HashMap<>();  //Dictionary in Python
         
         for(char m: magazine.toCharArray())     //put() & get() function in HashMap() -> .put(char,freq)
+        { 
             if(freq.containsKey(m))
                 freq.put(m, freq.get(m) + 1);
             else
                 freq.put(m,1);
-        
+        }
         for(char r: ransomNote.toCharArray())
+        { 
             if(freq.containsKey(r))
                 if(freq.get(r) > 1)
                     freq.put(r, freq.get(r) - 1);   //as we check remove it from magazine.
@@ -17,7 +19,7 @@ class Solution {
                     freq.remove(r);
             else
                 return false;
-                
+        }  
         return true;
     }
 }
