10519f155cc5ea67d08a65c63caef4dcec8667c6
Gayathri Ethiraj <39553592+gayu-thri@users.noreply.github.com>
2020-05-07 18:45:04 +0530

    Add Day4 - Bitwise Operators XOR^, left<< & right>>

--- May-Leetcoding-Challenge/4. Number Complement/NumberComplement.java
+++ May-Leetcoding-Challenge/4. Number Complement/NumberComplement.java
@@ -1,6 +1,15 @@
 class Solution {
     public int findComplement(int num)
     {
+        //num = 5 -> 101. Desired output is 010 which is 2.
+        //left shift 1 to perform XOR with the whole binary number
+        //101       100      110
+        //  1 ^      1  ^    1   ^
+        //-----     ----     ----
+        //100       110      010
+        //CONDITION : perform right shift in a temp variable assigned value as num >> 1 => 101 ; 10 ; 1 ; 0
+        //while temp ! = 0 perform xor.
+        
         int res = 0;
         int bit = 1;
         while(num > 0)
