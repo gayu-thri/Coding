3c9af38a93982e08334052bbe315ee74b4643c8c
gayu-thri <egayu3@gmail.com>
2020-12-14 20:39:21 +0530

    Update validatebst.py

--- Leetcode/validatebst.py
+++ Leetcode/validatebst.py
@@ -4,60 +4,37 @@ Created on Fri Jul 17 18:20:34 2020
 
 @author: egayu
 """
+import sys
 class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
+     def __init__(self, val):
          self.val = val
-         self.left = left
-         self.right = right
+         self.left = None
+         self.right = None
          
 def validatebst(root):
     #result = ['valid','not valid']
-    
-    
-    left = root.left
-    right = root.right
-    
-    if left and left.val > root.val:
-            return "not valid"
-    if right and right.val < root.val:
-            return "not valid"
-    res = validatebst(root.left) and validatebst(root.right)
-    return res
-'''
-# ANOTHER METHOD 
-import sys
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
+    def validate(root, MIN, MAX):
+        if root is None:
+            return True
         
-        def validate(root, MIN, MAX):
-            
-            if root is None:
-                return True
-            
-            if root.val >= MAX or root.val <= MIN:
-                return False
-            
-            return validate(root.left, MIN, root.val) and validate(root.right, root.val, MAX)
+        if root.val > MAX or root.val <= MIN:
+            return False
         
-        MIN, MAX = -sys.maxsize, sys.maxsize
-        return validate(root, MIN, MAX)
-'''
+        return validate(root.left, MIN, root.val) and validate(root.right, root.val, MAX)
+    
+    MIN, MAX = -sys.maxsize, sys.maxsize
+    return validate(root, MIN, MAX)
+
 
 #levelorder = [5,1,4,None,None,3,6]
 root = TreeNode(5)
 root.left = TreeNode(1)
-root.right = TreeNode(4)
-root.left.left = TreeNode(None)
-root.left.right = TreeNode(None)
+root.right = TreeNode(10)
+root.left.left = None
+root.left.right = None
 
-root.right.left = TreeNode(None)
-root.right.right = TreeNode(None)
+root.right.left = None
+root.right.right = None
 
 result = validatebst(root)
 print('Valid?>>>> ',result)
